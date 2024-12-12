d8cd34f514ab97229d43a514261468666e925322
Christopher Clemons <chris.clemons85@gmail.com>
2024-12-12 12:15:08 +0100

    feat(web): set up tailwind

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss'
+import sharedConfig from '@tapes-monorepo/tailwind-config'
+
+const config: Pick<Config, 'content' | 'presets' | 'theme'> = {
+  content: ['./app/**/*.tsx'],
+  presets: [sharedConfig],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+} satisfies Config
+
+export default config
