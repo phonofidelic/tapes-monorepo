9d269e1975cfa17765685d8c1136acc5a8b12496
Christopher Clemons <chris.clemons85@gmail.com>
2024-12-15 20:28:17 +0100

    fix(web): fix nav positioning, remove unused ids

--- apps/web/app/app/page.tsx
+++ apps/web/app/app/page.tsx
@@ -6,14 +6,14 @@ import DownloadPrompt from './DownloadPrompt'
 export default function AppPage() {
   return (
     <>
-      <div id="container" className="flex: sm:hidden">
+      <div className="flex sm:hidden">
         <ViewProvider>
           <SettingsProvider>
             <App />
           </SettingsProvider>
         </ViewProvider>
       </div>
-      <div className="mx-auto hidden size-full max-w-screen-sm flex-col items-center justify-center gap-16 sm:flex">
+      <div className="mx-auto hidden h-screen w-screen max-w-screen-sm flex-col items-center justify-center gap-16 sm:flex">
         <DownloadPrompt />
       </div>
     </>
