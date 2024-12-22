7627aa246f15104e403500f69c6215f340bb688a
Christopher Clemons <chris.clemons85@gmail.com>
2024-12-22 13:43:46 +0100

    refactor(core): move `TextInput` to ui package

--- packages/core/app/views/Recorder.tsx
+++ packages/core/app/views/Recorder.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 import { AiFillAudio, AiOutlineAudioMuted } from 'react-icons/ai'
 import { MdOutlineCancel, MdEdit, MdCheck } from 'react-icons/md'
 import { PiRecordFill } from 'react-icons/pi'
-import { Button } from '@tapes-monorepo/ui'
+import { Button, TextInput } from '@tapes-monorepo/ui'
 import { isValidAutomergeUrl } from '@automerge/automerge-repo'
 import { useDocument, useRepo } from '@automerge/automerge-repo-react-hooks'
 import { RecordingData, RecordingRepoState } from '@/types'
@@ -342,91 +342,6 @@ export function Recorder() {
         </div>
       </div>
     </>
-  )
-}
-
-function TextInput({
-  value,
-  defaultValue,
-  id,
-  type,
-  name,
-  label,
-  autofocus,
-  validate,
-  onChange,
-  onBlur,
-  onKeyDown,
-}: {
-  id: string
-  type: 'text' | 'email' | 'password'
-  name: string
-  label: string
-  autofocus?: boolean
-  value?: string
-  defaultValue?: string
-  validate?: (value: string) => string | undefined
-  onChange?(event: React.ChangeEvent<HTMLInputElement>): void
-  onBlur?(event: React.FocusEvent<HTMLInputElement>): void
-  onKeyDown?(event: React.KeyboardEvent<HTMLInputElement>): void
-}) {
-  const [error, setError] = useState<string | undefined>(undefined)
-
-  return (
-    <div className="relative w-full">
-      <input
-        value={value}
-        defaultValue={defaultValue}
-        id={id}
-        name={name}
-        type={type}
-        autoFocus={autofocus}
-        placeholder={label}
-        onChange={(event) => {
-          setError(undefined)
-
-          if (typeof onChange !== 'function') {
-            return
-          }
-
-          if (typeof validate !== 'function') {
-            onChange(event)
-            return
-          }
-
-          const error = validate(event.target.value)
-
-          if (!error) {
-            onChange(event)
-            return
-          }
-
-          setError(error)
-          onChange(event)
-        }}
-        onBlur={onBlur}
-        onKeyDown={onKeyDown}
-        className={clsx(
-          'peer w-full rounded p-2 text-zinc-800 placeholder-transparent outline-none dark:bg-zinc-900 dark:text-white',
-          {
-            'outline-zinc-400': error === undefined,
-            'outline-rose-500': error !== undefined,
-          },
-        )}
-      />
-      <label
-        htmlFor={id}
-        className={clsx(
-          'absolute -top-4 left-2 bg-white p-1 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:p-0 peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-focus:-top-4 peer-focus:p-1 peer-focus:text-sm dark:bg-zinc-900',
-          {
-            'text-zinc-400 peer-focus:text-zinc-400': error === undefined,
-            'text-rose-500 peer-focus:text-rose-500': error !== undefined,
-          },
-        )}
-      >
-        {error ?? label}
-      </label>
-    </div>
   )
 }
 
