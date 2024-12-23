3f9c4e1d32155008fbd5153b610e8572ea81d387
Christopher Clemons <chris.clemons85@gmail.com>
2024-12-24 00:36:52 +0100

    feat(core): add recording editor in Library view

--- packages/core/app/views/Library.tsx
+++ packages/core/app/views/Library.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { clsx } from 'clsx'
 import { AutomergeUrl, isValidAutomergeUrl } from '@automerge/automerge-repo'
 import { useDocument } from '@automerge/automerge-repo-react-hooks'
@@ -6,18 +6,21 @@ import {
   MdEdit,
   MdOutlineMoreVert,
   MdOutlineRemoveCircleOutline,
+  MdCheck,
 } from 'react-icons/md'
 import { Button, TextInput } from '@tapes-monorepo/ui'
 import { RecordingData, RecordingRepoState } from '@/types'
 import { useSetting } from '@/context/SettingsContext'
 import { useAppContext } from '@/context/AppContext'
-import { IpcResponse } from '@/IpcService'
+import { EditRecordingResponse, IpcResponse } from '@/IpcService'
 
 export function Library() {
   const [autoMergeUrl] = useSetting('automergeUrl')
   const [docState, changeDocState] = useDocument<RecordingRepoState>(
     isValidAutomergeUrl(autoMergeUrl) ? autoMergeUrl : undefined,
   )
+
+  const [editingUrl, setEditingUrl] = useState<AutomergeUrl | null>(null)
 
   const deleteRecording = (url: AutomergeUrl) => {
     changeDocState((doc) => {
@@ -28,18 +31,103 @@ export function Library() {
   }
 
   return (
-    <div className="flex flex-col">
-      <ul>
-        {docState?.recordings.map((url) => {
-          return (
-            <LibraryListItem
-              key={url}
-              automergeUrl={url}
-              onDelete={deleteRecording}
-            />
-          )
-        })}
-      </ul>
+    <>
+      <div className="flex flex-col">
+        <ul>
+          {docState?.recordings.map((url) => {
+            return (
+              <LibraryListItem
+                key={url}
+                automergeUrl={url}
+                onDelete={deleteRecording}
+                onOpenEditor={() => {
+                  setEditingUrl(url)
+                }}
+              />
+            )
+          })}
+        </ul>
+      </div>
+      <div
+        className={clsx(
+          'absolute bottom-0 left-0 z-50 w-screen rounded-t-lg border border-zinc-100 bg-white p-5 drop-shadow-2xl transition-transform dark:border-zinc-800 dark:bg-zinc-900',
+          {
+            'translate-y-0': editingUrl,
+            'translate-y-full': !editingUrl,
+          },
+        )}
+      >
+        <Editor automergeUrl={editingUrl} onClose={() => setEditingUrl(null)} />
+      </div>
+      <Backdrop
+        title="Close editor"
+        isOpen={editingUrl !== null}
+        onClose={() => setEditingUrl(null)}
+      />
+    </>
+  )
+}
+
+function Editor({
+  automergeUrl,
+  onClose,
+}: {
+  automergeUrl: AutomergeUrl | null
+  onClose: () => void
+}) {
+  const appContext = useAppContext()
+  const [recording, changeRecording] = useDocument<RecordingData>(
+    automergeUrl ?? undefined,
+  )
+
+  if (!recording) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div className="flex size-full flex-col gap-2">
+      <div className="flex justify-between gap-1">
+        <EditableText
+          text={recording.name}
+          label="Edit recording name"
+          onChange={(newName) => {
+            changeRecording((recording) => {
+              recording.name = newName
+            })
+          }}
+        />
+        <Button
+          className="rounded-full p-2 hover:text-green-500"
+          onClick={() => onClose()}
+        >
+          <MdCheck />
+        </Button>
+      </div>
+      <div className="flex gap-1 text-xs">
+        <div className="p-1">Filename:</div>
+        <EditableText
+          text={recording.filename}
+          label="Edit recording filename"
+          onChange={async (newName) => {
+            if (appContext.type !== 'electron-client') {
+              return
+            }
+            const editFilenameResponse =
+              await appContext.ipc.send<EditRecordingResponse>(
+                'storage:edit-recording',
+                { data: { filename: newName, filepath: recording.filepath } },
+              )
+            if (!editFilenameResponse.success) {
+              console.error(editFilenameResponse.error)
+              return
+            }
+            changeRecording((recording) => {
+              recording.filename = newName
+              recording.filepath = editFilenameResponse.data.filepath
+            })
+          }}
+        />
+      </div>
     </div>
   )
 }
@@ -47,9 +135,11 @@ export function Library() {
 function LibraryListItem({
   automergeUrl,
   onDelete,
+  onOpenEditor,
 }: {
   automergeUrl: AutomergeUrl
   onDelete: (url: AutomergeUrl) => void
+  onOpenEditor: () => void
 }) {
   const appContext = useAppContext()
   const [recording, changeRecording] = useDocument<RecordingData>(automergeUrl)
@@ -59,18 +149,7 @@ function LibraryListItem({
   const optionsMenuRef = useRef<HTMLDivElement | null>(null)
 
   const [isOptionsMenuOpen, setIsOptionsMenuOpen] = useState(false)
-  const [isEditing, setIsEditing] = useState(false)
-  const [editedName, setEditedName] = useState(recording?.name)
-  const [hasErrors, setHasErrors] = useState(false)
-
-  const formattedDuration = useMemo(() => {
-    if (!recording) {
-      return ''
-    }
-    const minutes = Math.floor(recording.duration / 6000)
-    const seconds = Math.floor(recording.duration % 6000)
-    return `${minutes}:${seconds.toString().padStart(2, '0')}`
-  }, [recording])
+  const [, setEditedName] = useState(recording?.name)
 
   useEffect(() => {
     if (!recording || initialized.current) {
@@ -88,49 +167,16 @@ function LibraryListItem({
 
   return (
     <>
-      <div className="group flex w-full justify-between p-4">
-        {isEditing ? (
-          <div className="w-full p-[7px]">
-            <TextInput
-              id="new-recording-name-input"
-              name="new-recording-name"
-              type="text"
-              label="Edit recording name"
-              defaultValue={editedName}
-              autofocus={true}
-              validate={(value) => {
-                // TODO: update validation regex
-                const hasErrors = /[^a-z0-9\s_@()-]/i.test(value)
-                setHasErrors(hasErrors)
-                return hasErrors ? 'Invalid characters' : undefined
-              }}
-              onChange={(event) => setEditedName(event.target.value)}
-              onBlur={() => {
-                if (!editedName) {
-                  setEditedName(previousRecordingName.current)
-                }
-                setIsEditing(false)
-              }}
-              onKeyDown={(event) => {
-                if (event.key === 'Enter') {
-                  if (!hasErrors && editedName && editedName.length > 0) {
-                    changeRecording((recording) => {
-                      recording.name = editedName
-                    })
-                  }
-                  setIsEditing(false)
-                }
-              }}
-            />
-          </div>
-        ) : (
-          <Button className="p-1" onClick={() => setIsEditing(true)}>
-            <p className="max-w-52 overflow-hidden text-ellipsis text-nowrap">
-              {recording.name}
-            </p>
-            <MdEdit className="ml-2 opacity-0 transition-opacity ease-in group-hover:opacity-100" />
-          </Button>
-        )}
+      <div className="group flex w-full justify-between py-4">
+        <EditableText
+          text={recording.name}
+          label="Edit recording name"
+          onChange={(newName) => {
+            changeRecording((recording) => {
+              recording.name = newName
+            })
+          }}
+        />
         <div className="flex gap-2">
           <p className="flex items-center text-xs text-zinc-400">
             <FormattedTime time={recording.duration} />
@@ -166,20 +212,21 @@ function LibraryListItem({
                   },
                 )}
               >
-                <li className="size-full rounded p-2 hover:bg-zinc-100 dark:hover:bg-zinc-800">
+                <li>
                   <Button
-                    className="flex gap-2"
+                    className="flex size-full gap-2 rounded p-2 hover:bg-zinc-100 dark:hover:bg-zinc-800"
                     onClick={() => {
                       setIsOptionsMenuOpen(false)
+                      onOpenEditor()
                     }}
                   >
                     <MdEdit /> Edit
                   </Button>
                 </li>
                 {appContext.type === 'electron-client' && (
-                  <li className="size-full rounded p-2 hover:bg-zinc-100 hover:text-rose-500 dark:hover:bg-zinc-800">
+                  <li>
                     <Button
-                      className="flex gap-2"
+                      className="flex size-full gap-2 rounded p-2 hover:bg-zinc-100 hover:text-rose-500 dark:hover:bg-zinc-800"
                       onClick={async () => {
                         const deleteRecordingResponse =
                           await appContext.ipc.send<IpcResponse>(
@@ -188,10 +235,17 @@ function LibraryListItem({
                               data: { filepath: recording.filepath },
                             },
                           )
-                        if (deleteRecordingResponse.error) {
+                        if (!deleteRecordingResponse.success) {
                           console.error(deleteRecordingResponse.error)
-                          setIsOptionsMenuOpen(false)
-                          return
+                          if (
+                            !deleteRecordingResponse.error.message.includes(
+                              'ENOENT',
+                            )
+                          ) {
+                            setIsOptionsMenuOpen(false)
+                            return
+                          }
+                          // If the file doesn't exist, remove it from the list
                         }
                         onDelete(automergeUrl)
                         setIsOptionsMenuOpen(false)
@@ -206,16 +260,10 @@ function LibraryListItem({
           </div>
         </div>
       </div>
-      <button
-        title={isOptionsMenuOpen ? 'Close menu' : ''}
-        className={clsx(
-          'absolute left-0 top-0 flex h-full w-screen bg-white transition-opacity ease-in-out dark:bg-zinc-900',
-          {
-            'hidden opacity-0': !isOptionsMenuOpen,
-            'z-40 opacity-50': isOptionsMenuOpen,
-          },
-        )}
-        onClick={() => setIsOptionsMenuOpen(false)}
+      <Backdrop
+        title="Close menu"
+        isOpen={isOptionsMenuOpen}
+        onClose={() => setIsOptionsMenuOpen(false)}
       />
     </>
   )
@@ -228,4 +276,100 @@ function FormattedTime({ time }: { time: number }) {
   const hours = ('0' + Math.floor(time / 3600000)).slice(-2)
 
   return `${hours}:${minutes}:${seconds}`
+}
+
+function EditableText({
+  text,
+  label,
+  onChange,
+}: {
+  text: string
+  label: string
+  onChange: (text: string) => void
+}) {
+  const previousTextRef = useRef(text)
+  const [isEditing, setIsEditing] = useState(false)
+  const [editedText, setEditedText] = useState(text)
+  const [hasErrors, setHasErrors] = useState(false)
+
+  return isEditing ? (
+    <div className="w-full p-[7px]">
+      <TextInput
+        id="new-recording-name-input"
+        name="new-recording-name"
+        type="text"
+        label={label}
+        defaultValue={editedText}
+        autofocus={true}
+        validate={(value) => {
+          // TODO: update validation regex
+          const hasErrors = /[^a-z0-9\s_@()-]/i.test(value)
+          setHasErrors(hasErrors)
+          return hasErrors ? 'Invalid characters' : undefined
+        }}
+        onChange={(event) => setEditedText(event.target.value)}
+        onBlur={() => {
+          if (!editedText) {
+            setEditedText(previousTextRef.current)
+          }
+          if (!hasErrors && editedText && editedText.length > 0) {
+            onChange(editedText)
+          }
+          setIsEditing(false)
+        }}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter') {
+            if (!hasErrors && editedText && editedText.length > 0) {
+              onChange(editedText)
+            }
+            setIsEditing(false)
+          }
+        }}
+      />
+    </div>
+  ) : (
+    <Button className="p-1" onClick={() => setIsEditing(true)}>
+      <p className="max-w-52 overflow-hidden text-ellipsis text-nowrap">
+        {text}
+      </p>
+      <MdEdit className="ml-2 opacity-0 transition-opacity ease-in group-hover:opacity-100" />
+    </Button>
+  )
+}
+
+function Backdrop({
+  isOpen,
+  title = 'Close',
+  onClose,
+}: {
+  isOpen: boolean
+  title?: string
+  onClose: () => void
+}) {
+  useEffect(() => {
+    const onEscape = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose()
+      }
+    }
+    document.addEventListener('keydown', onEscape)
+
+    return () => {
+      document.removeEventListener('keydown', onEscape)
+    }
+  }, [])
+
+  return (
+    <button
+      title={isOpen ? title : ''}
+      className={clsx(
+        'absolute left-0 top-0 flex h-full w-screen bg-white transition-opacity ease-in-out dark:bg-zinc-900',
+        {
+          'hidden opacity-0': !isOpen,
+          'z-40 opacity-50': isOpen,
+        },
+      )}
+      onClick={() => onClose()}
+    />
+  )
 }
