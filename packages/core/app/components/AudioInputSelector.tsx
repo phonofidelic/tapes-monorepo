6a8ea0cefa780caf1d564907b5462c10149bbe6f
Christopher Clemons <chris.clemons85@gmail.com>
2026-07-26 18:05:53 +0200

    fix(core): include whole stringified MediaDeviceInfo object in options
    TODO: validate `parsedMediaDeviceInfo` before using its values

--- packages/core/app/components/AudioInputSelector.tsx
+++ packages/core/app/components/AudioInputSelector.tsx
@@ -79,14 +79,19 @@ export function AudioInputSelector({ className }: { className?: string }) {
           setAudioInputDeviceId('')
           return
         }
+
+        // TODO: validate parsedMediaDeviceInfo
+        const parsedMediaDeviceInfo = JSON.parse(event.target.value) as MediaDeviceInfo
+
         if (appContext.type === 'electron-client') {
           try {
+
             const setAudioInputDeviceResponse =
               await appContext.ipc.send<IpcResponse>(
                 'settings:set-default-audio-input-device',
                 {
                   data: {
-                    deviceName: event.target.value,
+                    deviceName: parsedMediaDeviceInfo.label
                   },
                 },
               )
@@ -97,19 +102,15 @@ export function AudioInputSelector({ className }: { className?: string }) {
             console.error('Error setting default audio input device:', error)
           }
         }
-        setAudioInputDeviceId(event.target.value)
+        setAudioInputDeviceId(parsedMediaDeviceInfo.deviceId)
       }}
-      defaultValue={audioInputDeviceId ?? ''}
+      defaultValue={JSON.stringify(audioInputDevices.find(device => device.deviceId === audioInputDeviceId)) ?? ''}
     >
       <option value="">Select an audio input device</option>
       {audioInputDevices.map((device) => (
         <option
           key={device.deviceId}
-          value={
-            appContext.type === 'electron-client'
-              ? device.label
-              : device.deviceId
-          }
+          value={JSON.stringify(device)}
         >
           {device.label}
         </option>
